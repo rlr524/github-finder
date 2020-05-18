0219c813ff305d6bf811f72c77c965a8da18c9cc
Rob Ranf <robranf@gmail.com>
2020-05-18 14:44:15 -0700

    adding emiya link in about

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -6,6 +6,10 @@ const About = () => {
       <h1>About this App</h1>
       <p>App to search GitHub users</p>
       <p>Version 1.0.0</p>
+      <p>
+        Built by Rob @{" "}
+        <a href="https://www.emiyaconsulting.com">Emiya Consulting</a>
+      </p>
     </Fragment>
   );
 };
